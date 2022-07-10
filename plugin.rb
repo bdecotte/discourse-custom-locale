e56be2bcf2e98c7a087d984ceb5637415bfe888b
bdecotte <57888690+bdecotte@users.noreply.github.com>
2022-07-10 23:02:25 +0200

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: custom-locales
+# about: An example plugin for adding new locales.
+# version: 1.0
+
+register_locale("es_XX", name: "Spanish (Extra)", nativeName: "Español (Extra)", fallbackLocale: "es")
